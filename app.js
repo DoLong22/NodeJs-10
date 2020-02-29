d0487248dae587646d69aef66d3fd7cb0b70d44b
Long Do <doduclong2208@gmail.com>
2020-02-29 09:05:40 +0700

    thay dest(chỉ dùng để chỉ vị trí để lưu file) bằng storage(chỉ vị trí và tên file lưu)

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 
 const multer = require('multer');
-const upload = multer({dest:'./public'})
+const storage = multer.diskStorage({
+    destination:(req,file,cb) =>{
+        cb(null,'./public');
+    },
+    filename:(req,file,cb)=>{cb(null,'a.png')}
+})
+// const upload = multer({dest:'./public'})
+const upload = multer({storage:storage});
 
  const reload = require('reload');
 
