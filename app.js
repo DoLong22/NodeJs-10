018ea92b5d6e9fd1ce94f8dc18edcbf4bbbfcf61
Long Do <doduclong2208@gmail.com>
2020-02-29 08:52:39 +0700

    multer

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const multer = require('multer');
+const upload = multer({dest:'./public'})
+
+ const reload = require('reload');
+
+const app = express();
+
+app.set('views','./views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req,res)=>{
+    res.render('index');
+});
+
+app.post('/signup',upload.single('profile'),(req,res)=>{
+    res.send(req.body);    
+})
+reload(app);
+
+app.listen(3000,()=>console.log('Server started'));
